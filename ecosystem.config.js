6e4ed48f0fcf6dce55e54fcdeefda8586240a128
Anvar Turakulov <anvarsam@mail.ru>
2025-03-17 10:45:38 +0500

    fix ecosystems

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,8 +11,8 @@ module.exports = {
     },
     {
       name: "sh-front",
-      script: "npm",
-      args: "start",
+      script: "node",
+      args: ".next/standalone/server.js",
       cwd: "/home/anvar/od/od-shavkatnon/frontend",
       env: {
         NODE_ENV: "production"
